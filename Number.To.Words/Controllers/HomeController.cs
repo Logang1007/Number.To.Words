7eaf3c11c8472de8858a85342d2fddb09b1a7554
Logan Govender <logangovender0108@gmail.com>
2022-08-17 19:04:43 +0200

    Initial checkin for master

---
+++ Number.To.Words/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Number.To.Words.Models;
+
+
+namespace Number.To.Words.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult ConvertToWord(string number)
+        {
+            ViewData["NumberEntered"] = number;
+            
+            if (string.IsNullOrEmpty(number))
+            {
+                ViewData["Error"] = "Please enter a number";
+                return View("Index");
+            }
+            int numberToConvert = 0;
+            bool canParse = int.TryParse(number, out numberToConvert);
+            if (!canParse)
+            {
+                ViewData["Error"] = "Only numbers are allowed, no decimal values allowed";
+            }
+            else
+            {
+                ViewData["NumberToWords"] = NumberHelper.ToWords(numberToConvert);
+            }
+            
+         
+            return View("Index");
+        }
+    }
+}
---
+++ Number.To.Words/Helpers/NumberHelper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Number.To.Words
+{
+    public class NumberHelper
+    {
+
+        public static string ToWords(Int64 number, string appendScale = "")
+        {
+            var wordConverter = new WordConverterParams().ConverterParams;
+            string numString = "";
+       
+            if (number < 100)
+            {
+                if (number < 20)
+                    numString = wordConverter.OnesList[number];
+                else
+                {
+                    numString = wordConverter.TensList[number / 10];
+                    if ((number % 10) > 0)
+                        numString = string.Format(wordConverter.TensFormat, numString, wordConverter.OnesList[number % 10]);
+                }
+            }
+            else
+            {
+                int pow = 0;
+                string powStr = ""; 
+
+                if (number < 1000)
+                { 
+                    pow = 100; 
+                    powStr = wordConverter.ThousandList[0];
+                }
+                else
+                { 
+                    int log = (int)Math.Log(number, 1000);
+              
+                    pow = (int)Math.Pow(1000, log);
+          
+                    powStr = wordConverter.ThousandList[log];
+                }
+
+  
+                numString = string.Format(wordConverter.LargeSmallComboFormat, ToWords(number / pow, powStr), ToWords(number % pow)).Trim();
+            }
+
+            return string.Format(wordConverter.ScaleFormat, numString, appendScale).Trim();
+        }
+
+
+   
+    }
+
+
+    class WordConverterParams
+    {
+       
+        public (string[] OnesList, string[] TensList, string[] ThousandList, string TensFormat, string LargeSmallComboFormat, string ScaleFormat) ConverterParams
+        {
+            get
+            {
+                string[] onesList = {
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
+};
+
+                string[] tensList = { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                string[] thousandList = { "hundred", "thousand", "million", "billion", "trillion", "quadrillion" };
+
+                string tensFormat = "{0}-{1}"; 
+                string largeSmallComboFormat = "{0} {1}"; 
+                string scaleFormat = "{0} {1}"; 
+                return (onesList, tensList, thousandList, tensFormat, largeSmallComboFormat, scaleFormat);
+            }
+
+        }
+    }
+}
